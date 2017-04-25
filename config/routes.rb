bafade9bdb64a3341e615e11ece6c6a16bd0e06a
yoko <yoko.cookiemonster718@gmail.com>
2017-04-25 00:24:23 +0000

    init Rails Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #  get 'tasks/:id', to: 'tasks#show'
+   # post 'tasks', to: 'tasks#create'
+    #put 'tasks/:id', to: 'tasks#update'
+    #delete 'tasks/:id', to: 'tasks#destroy'
+    
+    # index: show の補助ページ
+#    get 'tasks', to: 'tasks#index'
+    
+    # new: 新規作成用のフォームページ
+ #   get 'tasks/new', to: 'tasks#new'
+  
+  # edit: 更新用のフォームページ
+  #  get 'tasks/:id/edit', to: 'tasks#edit'
+    
+    root to: 'tasks#index'
+    
+    resources :tasks
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170424071459) do
 
-  create_table "tasks", force: :cascade do |t|
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
