4792451926272ee9c2328b14ed29a4750a2bba14
yoko <yoko.cookiemonster718@gmail.com>
2017-04-25 08:02:31 +0000

    fixed3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content,:title)
+        params.require(:task).permit(:content,:status)
     end
     
 end
