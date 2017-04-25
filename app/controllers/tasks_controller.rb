00271dacd1d17af555fdeb81ad425900ad4d6bf8
yoko <yoko.cookiemonster718@gmail.com>
2017-04-25 07:01:35 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content,:title)
     end
     
 end
